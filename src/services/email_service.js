50c712738fbef5fd86827124b9bc9ed8568f432c
Balram <ashishbhangde.linkites@gmail.com>
2021-01-23 14:41:14 +0530

    'somechanges'

--- src/services/email_service.js
+++ src/services/email_service.js
@@ -1,10 +1,31 @@
 import _ from 'lodash'
 import config from '../config'
 
-const sendGrid = require('sendgrid')(config.get('/sendgrid').apiKey)
-
-class EmailService {
+// const sendGrid = require('sendgrid')(config.get('/sendgrid').apiKey)
+let sendGrid = require('@sendgrid/mail')
+sendGrid.setApiKey(process.env.SEND_GRID)
+const EmailService = {
+    sendMail(email,callback)
+		{
+			var data={
+	        	from : 'social@myskinuniverse.com',
+	        	to : "balramchawda@linkites.com",
+	        	subject :'Testing email',
+	        	html: `Testing email`
+	    	}
+    	  sendGrid.send(data)
+    .then(m => {
+      console.log('Mail sent')
+        callback(true)
+    })
+    .catch(error => {
+      console.error(error.toString())
+     callback(false)
+    })
+	}
 
 }
 
-export default EmailService
+module.exports = EmailService
+
+// export default EmailService
